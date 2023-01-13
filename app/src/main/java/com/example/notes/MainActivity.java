204c52273cb530ef3fc24818b1449461772c9f07
kashmalvi <113088465+kashmalvi@users.noreply.github.com>
2023-01-13 12:58:34 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.common.io.LineReader;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.Query;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton addNoteButton;
+    RecyclerView recyclerView;
+    ImageButton menuBtn;
+    NoteAdapter noteAdapter;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        //adding the reference of add note button
+        addNoteButton = findViewById(R.id.add_note_btn);
+        
+        //adding the reference of recycler view
+        recyclerView = findViewById(R.id.recycler_view);
+        menuBtn = findViewById(R.id.menu_btn);
+        
+        addNoteButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, NoteDetailsActivity.class)));
+        menuBtn.setOnClickListener((v)-> showMenu());
+        setupRecyclerView();
+    }
+
+    void showMenu(){
+        //to display menu
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this, menuBtn);
+        popupMenu.getMenu().add("LogOut");
+        popupMenu.show();
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if(menuItem.getTitle() == "LogOut"){
+                    //make logout from firebase firebaseAuth
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    void setupRecyclerView(){
+        //for getting the notes from fire store we need the collection reference
+        Query query = Utility.getCollectionReferenceForNotes().orderBy("timestamp", Query.Direction.DESCENDING);
+                            //in the direction of descending order
+
+        //now converting the query into options
+        FirestoreRecyclerOptions<Note> option = new FirestoreRecyclerOptions.Builder<Note>()
+                .setQuery(query, Note.class).build();
+                //this will return the recycler option with query with note
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter = new NoteAdapter(option, this);
+        recyclerView.setAdapter(noteAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        noteAdapter.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        noteAdapter.notifyDataSetChanged(); //after adding the note recycler view show be updated and displayed
+    }
+}
